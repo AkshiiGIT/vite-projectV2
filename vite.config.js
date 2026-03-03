3ab4fe422aa797498b35b82fd375286993f20f6f
AkshiiGIT <ton-email-github@exemple.com>
2026-03-03 12:28:58 +0100

    Deploiement du showroom BMW

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Très important pour que tes images et modèles 3D chargent sur GitHub
+  base: './', // Nécessaire pour le chargement des ressources sur GitHub
 })
